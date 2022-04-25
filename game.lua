52a7238f0b3c4a8f93c2adf5adc3d9ec4ebc9000
Minori <similianka@gmail.com>
2022-04-25 14:02:00 +1000

    massive

---
+++ game.lua
@@ -0,0 +1,196 @@
+PLAYER_COLOR       = {133/255,100/255,123/255, 1}
+BALL_COLOR         = PLAYER_COLOR
+SCORE_COLOR        = PLAYER_COLOR
+DEADZONE_COLOR     = {0, 0.8, 0}
+BACKGROUND_COLOR   = {242/255, 220/255, 224/255}
+
+PLAYER_HEIGHT = 90
+PLAYER_WIDTH = 20
+PLAYER_VELOCITY = 8
+DEADZONE_WIDTH = 10
+PLAYER_X_OFFSET = DEADZONE_WIDTH + 5
+
+local game = {}
+
+local
+function clamp(min, val, max)
+    return math.max(min, math.min(val, max));
+end
+
+function game.reset_ball()
+    game.ball = {}
+    game.ball.x = window_width/2
+    game.ball.y = window_height/2
+    -- TODO: random direction
+    game.ball.vx = 2
+    game.ball.vy = 2
+    game.ball.radius = 20
+end
+
+local
+function player(x)
+    return
+    {
+        x = x,
+        y = 0,
+        width = PLAYER_WIDTH,
+        height = PLAYER_HEIGHT,
+        score = 0
+    }
+end
+
+function game.reset_players()
+    game.players = {}
+    game.players.left  = player(PLAYER_X_OFFSET)
+    game.players.right = player(window_width - PLAYER_WIDTH - PLAYER_X_OFFSET)
+end
+
+function game.reset_players_position()
+    game.players.left.x = PLAYER_X_OFFSET
+    game.players.right.x = window_width - PLAYER_WIDTH - PLAYER_X_OFFSET
+end
+
+function game.init_deadzones()
+    game.deadzones = {}
+    game.deadzones.left = {x = 0, y = 0, width = DEADZONE_WIDTH, height = window_height}
+    game.deadzones.right = {x = window_width - DEADZONE_WIDTH, y = 0, width = DEADZONE_WIDTH, height = window_height}
+end
+
+function game.init()
+    game.reset_ball()
+    game.reset_players()
+    game.init_deadzones()
+end
+
+function game.draw_players()
+    love.graphics.setColor(unpack(PLAYER_COLOR))
+    love.graphics.rectangle("fill", game.players.left.x, game.players.left.y, PLAYER_WIDTH, PLAYER_HEIGHT)
+    love.graphics.rectangle("fill", game.players.right.x, game.players.right.y, PLAYER_WIDTH, PLAYER_HEIGHT)
+end
+
+function game.draw_ball()
+    love.graphics.setColor(unpack(BALL_COLOR))
+    love.graphics.circle("fill", game.ball.x, game.ball.y, game.ball.radius)
+end
+
+function game.draw_deadzones()
+    love.graphics.setColor(unpack(DEADZONE_COLOR))
+    love.graphics.rectangle("fill", 0, 0, DEADZONE_WIDTH, window_height)
+    love.graphics.rectangle("fill", window_width - DEADZONE_WIDTH, 0, DEADZONE_WIDTH, window_height)
+end
+
+function game.draw_score()
+    love.graphics.setColor(unpack(SCORE_COLOR))
+    local offset = 150
+    score = {}
+    score.width = 50
+    score.height = 50
+    score.y = 50
+
+    -- love.graphics.rectangle("fill", window_width * 0.5 - score.width - offset, score.y,
+    --                                 score.width, score.height)
+
+    -- love.graphics.rectangle("fill", window_width * 0.5 + offset, score.y,
+    --                                 score.width, score.height)
+
+	local font = love.graphics.newFont(60)
+    love.graphics.print(game.players.left.score,
+                        font,
+                        window_width * 0.5 - score.width - offset,
+                        score.y)
+
+    love.graphics.print(game.players.right.score,
+                        font,
+                        window_width * 0.5 + offset,
+                        score.y)
+end
+
+function game.draw_middle_line()
+    love.graphics.setColor(unpack(PLAYER_COLOR))
+    local line_width = 6
+    love.graphics.rectangle("fill", (window_width - line_width) * 0.5, 0, line_width, window_height)
+end
+
+function game.update_players()
+    if love.keyboard.isDown ('w') then
+        game.players.left.y = game.players.left.y - PLAYER_VELOCITY
+    end
+    if love.keyboard.isDown ('s') then
+        game.players.left.y = game.players.left.y + PLAYER_VELOCITY
+    end
+    game.players.left.y = clamp(0, game.players.left.y, window_height - PLAYER_HEIGHT)
+
+    if love.keyboard.isDown ('up') then
+        game.players.right.y = game.players.right.y - PLAYER_VELOCITY
+    end
+    if love.keyboard.isDown ('down') then
+        game.players.right.y = game.players.right.y + PLAYER_VELOCITY
+    end
+    game.players.right.y = clamp(0, game.players.right.y, window_height - PLAYER_HEIGHT)
+end
+
+local
+function ball_rect_intersect(ball, rect)
+
+    local ball_distance = {}
+    local rect_center = {x = rect.x + rect.width/2,
+                         y = rect.y + rect.height/2}
+    ball_distance.x = math.abs(ball.x - rect_center.x)
+    ball_distance.y = math.abs(ball.y - rect_center.y)
+
+    if (ball_distance.x > (rect.width/2 + ball.radius)) then
+        return false
+    end
+
+    if (ball_distance.y > (rect.height/2 + ball.radius)) then
+        return false
+    end
+
+    if (ball_distance.x <= (rect.width/2)) then
+        return true
+    end
+
+    if (ball_distance.y <= (rect.height/2)) then
+        return true
+    end
+
+    local corner_distance_sq = (ball_distance.x - rect.width/2)^2 +
+                               (ball_distance.y - rect.height/2)^2
+
+    return (corner_distance_sq <= (ball.radius^2))
+end
+
+function game.update_ball()
+    -- reflecting ball when it hits floor or ceiling
+    -- TODO: clamp ball.y so is does not overflow or underflow
+    if game.ball.y >= window_height-game.ball.radius then
+        game.ball.vy = game.ball.vy*-1
+    end
+    if game.ball.y <= game.ball.radius then
+        game.ball.vy = game.ball.vy*-1
+    end
+
+    -- TODO: collision can occur multiple times if we have corner collision - need fix
+    if ball_rect_intersect(game.ball, game.players.left) or
+       ball_rect_intersect(game.ball, game.players.right) then
+        game.ball.vx = game.ball.vx*-1.2
+    end
+
+    if ball_rect_intersect(game.ball, game.deadzones.left) then
+        game.players.right.score = game.players.right.score + 1
+        game.reset_ball()
+        game.reset_players_position()
+    elseif ball_rect_intersect(game.ball, game.deadzones.right) then
+        game.players.left.score = game.players.left.score + 1
+        game.reset_ball()
+        game.reset_players_position()
+    end
+
+    game.ball.y = game.ball.y + game.ball.vy
+    game.ball.x = game.ball.x + game.ball.vx
+
+end
+
+game.init()
+game.current_state = "Main Menu"
+return game
--- main.lua
+++ main.lua
@@ -1,150 +1,188 @@
+--[[
 function love.mousepressed (_, _, button, _)
-	if button==1 and currentState=="menu" then 
-		currentState = "game"
-		circleVelocityX=2
-		circleVelocityY=2
-		circleX=windowWidth/2
-		circleY=windowHigth/2
-		y_second=0
-		y_first=0
+	if button==1 and current_state=="menu" then
+		current_state = "game"
+		ball.vx=2
+		ball.vy=2
+		ball.x=window_width/2
+		ball.y=window_height/2
+		game.players.right.y=0
+		game.players.left.y=0
 		isGameOver=false
 	end
 end
+
 function love.touchpressed (_, x, y)
-	if currentState=="menu" then 
-		currentState = "game"
-		circleVelocityX=2
-		circleVelocityY=2
-		circleX=windowWidth/2
-		circleY=windowHigth/2
-		y_second=0
-		y_first=0
+	if current_state=="menu" then
+		current_state = "game"
+		ball.vx=2
+		ball.vy=2
+		ball.x=window_width/2
+		ball.y=window_height/2
+		game.players.right.y=0
+		game.players.left.y=0
 		isGameOver=false
 	end
-	if x<=windowWidth/2 then
-	y_first = y - HigthOfRectangle/2
-		if y<=HigthOfRectangle/2 then y_first=0
+	if x<=window_width/2 then
+	game.players.left.y = y - PLAYER_HEIGHT/2
+		if y<=PLAYER_HEIGHT/2 then game.players.left.y=0
 		end
-		if y>=windowHigth-HigthOfRectangle/2 then y_first=windowHigth-HigthOfRectangle
+		if y>=window_height-PLAYER_HEIGHT/2 then game.players.left.y=window_height-PLAYER_HEIGHT
 		end
 	else
-	y_second = y - HigthOfRectangle/2
-		if y<=HigthOfRectangle/2 then y_second=0
+	game.players.right.y = y - PLAYER_HEIGHT/2
+		if y<=PLAYER_HEIGHT/2 then game.players.right.y=0
 		end
-		if y>=windowHigth-HigthOfRectangle/2 then y_second=windowHigth-HigthOfRectangle
+		if y>=window_height-PLAYER_HEIGHT/2 then game.players.right.y=window_height-PLAYER_HEIGHT
 		end
 	end
 end
 
 function love.touchmoved (_, x, y)
-	if x<=windowWidth/2 then
-	y_first = y - HigthOfRectangle/2
-		if y<=HigthOfRectangle/2 then y_first=0
+	if x<=window_width/2 then
+	game.players.left.y = y - PLAYER_HEIGHT/2
+		if y<=PLAYER_HEIGHT/2 then game.players.left.y=0
 		end
-		if y>=windowHigth-HigthOfRectangle/2 then y_first=windowHigth-HigthOfRectangle
+		if y>=window_height-PLAYER_HEIGHT/2 then game.players.left.y=window_height-PLAYER_HEIGHT
 		end
 	else
-	y_second = y - HigthOfRectangle/2
-		if y<=HigthOfRectangle/2 then y_second=0
+	game.players.right.y = y - PLAYER_HEIGHT/2
+		if y<=PLAYER_HEIGHT/2 then game.players.right.y=0
 		end
-		if y>=windowHigth-HigthOfRectangle/2 then y_second=windowHigth-HigthOfRectangle
+		if y>=window_height-PLAYER_HEIGHT/2 then game.players.right.y=window_height-PLAYER_HEIGHT
 		end
 	end
 end
+]]--
+
+BUTTON_HEIGHT = 64
+local
+function button(text, fun)
+    return
+    {
+        text = text,
+        fun = fun,
+
+        now = false,
+        last = false
+    }
+end
+
+local main_menu_buttons = {}
+local font = nil
+local game = {}
 
 function love.load ()
-	windowWidth, windowHigth = love.graphics.getDimensions()
-	love.window.setMode (windowWidth, windowHigth)
-	love.graphics.setNewFont(30)
-	y_second=0
-	velocity=4
-	y_first=0
-	HigthOfRectangle=90
-	circleX=windowWidth/2
-	circleY=windowHigth/2
-	circleR=20
-	circleVelocityX=2
-	circleVelocityY=2
-	multy=1
-	isGameOver=false
-	currentState = "menu"
+	window_width, window_height = love.graphics.getDimensions()
+	love.window.setMode(window_width, window_height)
+	love.window.setTitle("Pong client")
+	font = love.graphics.newFont(30)
+
+    game = require('game')
+
+    table.insert(main_menu_buttons, button(
+            "Local game",
+            function()
+                game.current_state = "Local Game"
+                game.init()
+            end))
+
+    table.insert(main_menu_buttons, button(
+            "Online game",
+            function()
+                love.event.quit(0)
+            end))
+
+    table.insert(main_menu_buttons, button(
+            "Exit",
+            function()
+                love.event.quit(0)
+            end))
+
 end
 
 function love.update (dt)
-	if currentState == "menu" then
+	if game.current_state == "Main Menu" then
 		return
 	end
-	if isGameOver then 
-		return
-	end
-	if circleY>=windowHigth-circleR then
-		circleVelocityY=circleVelocityY*-1
-	end
-	if circleY<=circleR then
-		circleVelocityY=circleVelocityY*-1
-	end
 
-	if circleX<=20+circleR then
-		if circleY+circleR < y_first+HigthOfRectangle and circleY+circleR > y_first then
-			circleVelocityX=circleVelocityX*-1.2
-		else
-			isGameOver = true
-		end
-	end
-	if circleX>=windowWidth-2*circleR then
-		if circleY+circleR < y_second+HigthOfRectangle and circleY+circleR > y_second then
-			circleVelocityX=circleVelocityX*-1.2	
-		else
-			isGameOver = true
-		end
-	end
+    if game.current_state == "Local Game" then
 
-	circleY=circleY+circleVelocityY*multy
-	circleX=circleX+circleVelocityX*multy
+        game.update_players()
+        game.update_ball()
 
-	if y_first>0 then
-		if love.keyboard.isDown ('up') then
-			y_first=y_first-velocity*2*multy
-		end	
-	end
-	if y_first<windowHigth-HigthOfRectangle then
-		if love.keyboard.isDown ('down') then
-			y_first=y_first+velocity*2*multy
-		end
-	end
-	if y_second>0 then
-		if love.keyboard.isDown ('w') then
-			y_second=y_second-velocity*2*multy
-		end
-	end
-	if y_second<windowHigth-HigthOfRectangle then
-		if love.keyboard.isDown ('s') then
-			y_second=y_second+velocity*2*multy
-		end	
-	end
+    end
+end
+
+-- offset from center
+local
+function draw_buttons(buttons, x_offset, y_offset, button_width)
+    local ww = window_width
+    local wh = love.graphics.getHeight()
+
+    local margin = 16
+    local cursor_y = 0
+
+    local total_height = (BUTTON_HEIGHT + margin) * #buttons;
+    for i, button in ipairs(buttons) do
+        button.last = button.now
+
+        button_x = 0.5 * (ww - button_width) + x_offset
+        button_y = 0.5 * (wh - total_height) + y_offset + cursor_y
+
+        local color = {0.5, 0.4, 0.5, 1}
+
+        local mouse_x, mouse_y = love.mouse.getPosition()
+
+        local hot = mouse_x > button_x and mouse_x < button_x + button_width and
+                    mouse_y > button_y and mouse_y < button_y + BUTTON_HEIGHT
+
+        if hot then
+            color = {1, 0.8, 1, 1}
+        end
+
+        button.now = love.mouse.isDown(1)
+
+        if hot and button.now and not button.last then
+            button.fun()
+        end
+
+        love.graphics.setColor(unpack(color))
+        love.graphics.rectangle(
+            "fill",
+            button_x,
+            button_y,
+            button_width,
+            BUTTON_HEIGHT
+        )
+
+        local text_width = font:getWidth(button.text)
+        local text_height = font:getHeight(button.text)
+
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.print(button.text,
+                            font,
+                            button_x + 0.5 * (button_width - text_width),
+                            button_y + text_height * 0.5)
+
+        cursor_y = cursor_y + BUTTON_HEIGHT + margin
+
+    end
 end
 
 function love.draw ()
-if currentState == "menu" then
-	love.graphics.setBackgroundColor(242/255, 200/255, 220/255)
-	love.graphics.setColor(133/255,100/255,123/255)
-	love.graphics.print("Start", windowWidth/2, windowHigth/2)
-	if circleX>=windowWidth-100  then 
-	 	love.graphics.print("Game over. Right player lost", windowWidth/3, windowHigth*5/6)
-	end
-	if circleX <= windowHigth-100 then
-		love.graphics.print("Game over. Left player lost", windowWidth/3, windowHigth*5/6)
-	end
-else
-	love.graphics.setBackgroundColor(242/255, 220/255, 224/255)
-	love.graphics.setColor(133/255,100/255,123/255)
-	love.graphics.rectangle("fill", 0, y_first, 20, HigthOfRectangle)
-	love.graphics.rectangle("fill", windowWidth-20, y_second, 20, HigthOfRectangle)
-	love.graphics.setColor(133/255,100/255,123/255)
-	love.graphics.circle("fill", circleX, circleY, circleR)
-	if isGameOver then 
-		currentState = "menu"
-		
-	end
-end
+    local ww = love.graphics.getWidth()
+    local wh = love.graphics.getHeight()
+
+    love.graphics.setBackgroundColor(unpack(BACKGROUND_COLOR))
+    if game.current_state == "Main Menu" then
+        draw_buttons(main_menu_buttons, 0, 0, ww/3)
+    else
+        game.draw_players()
+        game.draw_ball()
+        game.draw_deadzones()
+        game.draw_middle_line()
+        game.draw_score()
+
+    end
 end
